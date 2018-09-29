afabe0d67eae6ac75f5392829b56dc5577613679
Helmi Aziz <6540616+elmyrockers@users.noreply.github.com>
2018-09-30 05:18:07 +0800

    modify new website

---
+++ contact_form/contact_form.php
@@ -0,0 +1,10 @@
+<?php 
+
+$type = 'success';
+$message = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+echo json_encode(compact( 'type', 'message' ));
+
+
+
+// echo "Contact form successfully submitted. Thank you, I will get back to you soon!";
